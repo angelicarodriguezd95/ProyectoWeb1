45feebf4e5d02efff1a79aea26e9d325ef54d1ad
angelicarodriguezd95 <118750418+angelicarodriguezd95@users.noreply.github.com>
2022-11-22 07:57:52 -0500

    Add files via upload

---
+++ js/a13_4.js
@@ -0,0 +1,12 @@
+let base;
+let altura;
+let area;
+
+
+base = parseInt(prompt("Ingresar la base del triangulo",""));
+altura = parseInt(prompt("Ingresar la altura del triangulo",""));
+
+area = (base * altura)/ 2 ;
+
+
+document.write("<br> En área del truangulo es:" + area);
