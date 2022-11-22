3bb2615d3aaf51a545d1e299fdf9e38556fffc59
angelicarodriguezd95 <118750418+angelicarodriguezd95@users.noreply.github.com>
2022-11-22 07:58:48 -0500

    Add files via upload

---
+++ js/a13_5.js
@@ -0,0 +1,11 @@
+let soles;
+let dolares;
+let cambio;
+
+dolares= parseInt(prompt("Ingresar un valor en dólares",""));
+
+soles = (dolares*3.38);
+
+dolares = soles;
+
+document.write("<br> El equivalente en soles es: s/" + dolares);
