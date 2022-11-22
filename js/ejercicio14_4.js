73f37b161a2b5678089f5a14e7914f1d9c20a12e
angelicarodriguezd95 <118750418+angelicarodriguezd95@users.noreply.github.com>
2022-11-21 20:35:01 -0500

    Add files via upload

---
+++ js/ejercicio14_4.js
@@ -0,0 +1,18 @@
+let a=1;
+let b=5;
+let c=4;
+let d=6;
+
+let suma;
+let resta;
+let resultado;
+
+suma= a+b; 
+resta= d-c;
+
+resultado= suma*resta;
+
+alert("La multiplicacion es: " + resultado);
+
+
+
