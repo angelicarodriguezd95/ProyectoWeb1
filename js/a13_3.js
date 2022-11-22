806b83e1f8d36d037f6572cea3f59e100e0c746b
angelicarodriguezd95 <118750418+angelicarodriguezd95@users.noreply.github.com>
2022-11-22 07:56:58 -0500

    Add files via upload

---
+++ js/a13_3.js
@@ -0,0 +1,13 @@
+let metros;
+let cm;
+let mm;
+
+metros = parseInt(prompt("Ingrese el número que deseado a convertir en cm y mm:"));
+
+cm = metros*100;
+
+mm= cm*10;
+
+document.write("<br> En cm son:" + cm);
+document.write("<br>En mm son" + mm);
+        
