e88b06264c76f0765f9554545042c36220ad3c45
angelicarodriguezd95 <118750418+angelicarodriguezd95@users.noreply.github.com>
2022-11-22 21:54:47 -0500

    Add files via upload

---
+++ js/ejercicio16_1.js
@@ -0,0 +1,20 @@
+ let multiplicar = function (x,y){
+    return x*y
+ }
+
+ let expo =function (x,y){
+    return Math.pow(x,y);
+ }
+
+ let suma=(x,y)=> {return x+y;};
+
+ var resultado = multiplicar(5,2);
+ alert (result);
+
+ resultado = suma(3+4);
+ alert(result);
+
+ let resta =(x,y)=>{return x-y};
+
+ resultado = resta(5-3);
+ alert(result);
