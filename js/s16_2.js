a9740652329f50c9734d3235d16392fab2ed1c08
angelicarodriguezd95 <118750418+angelicarodriguezd95@users.noreply.github.com>
2022-11-22 22:27:11 -0500

    Add files via upload

---
+++ js/s16_2.js
@@ -0,0 +1,12 @@
+let pasaraFarenheit = (grado) => { return Fahsarenheit= grado*1.8+32;};
+
+var result = pasaraFarenheit (22);
+
+
+
+function convertirFarenheit() {
+var gradosCelsius = parseInt(document.getElementById("celsius").value);
+var resultadoCelsius = document.getElementById("farenheit");
+var farenheit= (gradosCelsius * (9/5)) + 32;
+alert(result);
+}
