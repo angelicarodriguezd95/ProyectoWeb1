a60e3619b3995b0d0160670dd27cf12632fcf63f
angelicarodriguezd95 <118750418+angelicarodriguezd95@users.noreply.github.com>
2022-11-22 22:48:16 -0500

    Add files via upload

---
+++ js/s16_3.js
@@ -0,0 +1,12 @@
+let num1;
+let num2;
+let suma;
+        
+num1 = parseInt(prompt('Ingrese el primer número:'));
+        
+num2 = parseInt(prompt('Ingrese el segundo número:'));
+        
+suma = num1 + num2;
+
+document.write('La suma es:' + suma);
+           
