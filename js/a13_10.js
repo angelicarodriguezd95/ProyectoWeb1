078ae836c473a552efc555f80efc55ffeac14ba9
angelicarodriguezd95 <118750418+angelicarodriguezd95@users.noreply.github.com>
2022-11-22 20:15:24 -0500

    Add files via upload

---
+++ js/a13_10.js
@@ -0,0 +1,25 @@
+let entradaG;
+let entradaMy;
+let entradaMen;
+
+let totalG;
+let totalMy;
+let totalMe;
+
+let recaudado;
+
+entradaG=  parseInt(prompt("Ingresar cantidad entradas generales",""));
+entradaMy=  parseInt(prompt("Ingresar cantidad entradas a mayores de edad",""));
+entradaMen= parseInt(prompt("Ingresar cantidad entradas  a menores de edad",""));
+
+totalG= entradaG*150;
+totalMy= entradaMy*50;
+totalMe= entradaMen*80;
+
+recaudado= totalG+totalMy+totalMe;
+
+document.write("<br>EL total de entradas generales es:  "+ totalG);
+document.write("<br>EL total de entradas para mayores de edad es:  "+ totalMy);
+document.write("<br>EL total de entradas para menores de edad es:  "+ totalMe);
+
+document.write("<br>EL total del monto recaudado es:  "+ recaudado);
